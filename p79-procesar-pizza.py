77ee75518fc75863e7ab87c8b1a08fb1b35046e2
FJ_LoTo <32135890@uaz.edu.mx>
2024-05-02 09:08:07 -0600

    Actividad 12

---
+++ p79-procesar-pizza.py
@@ -0,0 +1,18 @@
+import os; os.system('cls')
+
+ingr = {'T':1.50,'P':3.50,'C':3.74,'A':0.40}
+base = 15
+st = 0
+tot = 0
+
+pedido = input('ingredientes de tu pizza? ').upper()
+for i in pedido:
+    if i in 'PTCA':
+        st = st+ingr[i]
+st = st+base
+
+if st>=20:
+    tot = st-(st*0.05)
+
+print(f'el subtotal es {st}')
+print(f'\nel total es {tot}')
